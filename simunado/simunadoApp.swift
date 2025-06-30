8aac4bef75af581f9248f66b962c251dbbc6ddc2
Munareto <rafael.munareto@icoud.com>
2025-06-30 07:30:26 -0300

    Initial Commit

---
+++ simunado/simunadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  simunadoApp.swift
+//  simunado
+//
+//  Created by Munareto on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct simunadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
